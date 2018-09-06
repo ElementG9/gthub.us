33e6eeb26837151a204c9272b597ab5efc037eaa
ElementG9 <garentyler@gmail.com>
2018-09-05 20:35:54 -0600

    Added logging

--- client-scripts/dashboard.js
+++ client-scripts/dashboard.js
@@ -1,5 +1,6 @@
 var app = angular.module("OtherPosts", []);
 app.controller("OtherPostsCtl", function ($scope, $http) {
+    console.log($scope);
     $http.get("/feed/" + $scope.user).then((data) => {
         console.log(data);
         $scope.posts = data.data;

f97d5b97c77a06c638851148427f892e8b66eae5
ElementG9 <garentyler@gmail.com>
2018-09-05 20:26:29 -0600

    Fixed tutorial error

--- client-scripts/dashboard.js
+++ client-scripts/dashboard.js
@@ -1,6 +1,6 @@
 var app = angular.module("OtherPosts", []);
 app.controller("OtherPostsCtl", function ($scope, $http) {
-    $http.get("/feed/asdf").success((data) => {
+    $http.get("/feed/asdf").then((data) => {
         console.log(data);
         $scope.posts = data;
     });

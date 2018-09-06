3c4ee4bed08131c55017d052e01236553d72fa5f
ElementG9 <garentyler@gmail.com>
2018-09-05 20:30:30 -0600

    Added username passing

--- client-scripts/dashboard.js
+++ client-scripts/dashboard.js
@@ -1,6 +1,6 @@
 var app = angular.module("OtherPosts", []);
 app.controller("OtherPostsCtl", function ($scope, $http) {
-    $http.get("/feed/asdf").then((data) => {
+    $http.get("/feed/" + $scope.user).then((data) => {
         console.log(data);
         $scope.posts = data.data;
     });

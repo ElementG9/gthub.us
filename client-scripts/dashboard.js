267b806e29ddfed123f1f5bb2913cd16c2a838d7
ElementG9 <garentyler@gmail.com>
2018-09-10 19:38:39 -0600

    Made changes to content type

--- client-scripts/dashboard.js
+++ client-scripts/dashboard.js
@@ -4,14 +4,20 @@ app.controller("AppCtl", function ($scope, $http) {
     $scope.content = "";
     $scope.sendPost = () => {
         console.log("Sending post");
-        $http.post("/post", {
-            content: $scope.content
+        $http({
+            url: "/post",
+            data: {
+                content: $scope.content
+            },
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
         }).then(() => {
             console.log("Post success");
+            $scope.getPosts();
         }).catch(() => {
             console.log("Post error");
         });
-        $scope.getPosts();
     };
     $scope.getPosts = () => {
         console.log("Getting posts");

9afc9c3e1bcfd761c2cf026abf735ba0dc3918bd
ElementG9 <garentyler@gmail.com>
2018-09-10 19:19:45 -0600

    Made posting not refresh page

--- client-scripts/dashboard.js
+++ client-scripts/dashboard.js
@@ -1,8 +1,29 @@
-var app = angular.module("OtherPosts", []);
-app.controller("OtherPostsCtl", function ($scope, $http) {
-    console.log($scope);
-    $http.get("/feed").then((data) => {
-        console.log(data);
-        $scope.posts = data.data;
-    });
+var app = angular.module("App", []);
+app.controller("AppCtl", function ($scope, $http) {
+    // definitions
+    $scope.content = "";
+    $scope.sendPost = () => {
+        $http({
+            method: 'POST',
+            url: '/post',
+            data: {
+                content: $scope.content
+            },
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        $scope.getPosts();
+    };
+    $scope.getPosts = () => {
+        $http({
+            method: 'GET',
+            url: '/feed',
+        }).then((data) => {
+            $scope.posts = data.data;
+        });
+    };
+
+    // load the posts
+    $scope.getPosts();
 });

e6cc95d015d10a683437052cc6de886ba6b8cd22
ElementG9 <garentyler@gmail.com>
2018-09-11 20:15:04 -0600

    Added __dirname to readfilesync

--- server-scripts/dbCtrl.js
+++ server-scripts/dbCtrl.js
@@ -1,4 +1,4 @@
-const db = JSON.parse(require("fs").readFileSync("../config.json", "utf8")).db;
+const db = JSON.parse(require("fs").readFileSync(__dirname + "/../config.json", "utf8")).db;
 const mongoose = require("mongoose");
 var bcrypt = require("bcryptjs");
 

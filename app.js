ee7af49a157ac5b64869579b74e9490608be0038
ElementG9 <elementg9dev@gmail.com>
2018-12-13 17:58:52 -0700

    Removed all old things, and restarted. Installed dependencies and created basic files.

--- app.js
+++ app.js
@@ -1,60 +1,8 @@
+require('dotenv').config();
 const app = require("express")();
-const http = require("http").Server(app);
-var io = require("socket.io")(http);
-
-app.set("view engine", "pug");
-
 app.get("/", (req, res) => {
-    res.render("site-index", {
-        title: "gthub.us"
-    });
+    res.send("<h1>Hello world!</h1>");
 });
-
-/* - - - Start Twitter Clone - - - */
-var twitterRouter = require(__dirname + "/server-scripts/twitter-webserver.js");
-app.use("/twitter-clone/", twitterRouter);
-var protectRoute = (req, res, next) => {
-    if (req.session.user && req.cookies.user_sid) {
-        next(); // if logged in, continue
-    } else {
-        res.redirect("/twitter-clone/login"); // else redirect to login
-    }
-};
-// get the dashboard
-app.get("/twitter-clone/dashboard", protectRoute, (req, res) => { // the dashboard page
-    var user = req.session.user;
-    res.render("dashboard", {
-        project: "twitterclone",
-        loggedin: true,
-        username: user.username,
-        title: "gthub.us Dashboard"
-    });
-});
-io.on("connection", (socket) => {
-    socket.on("create post", () => {
-        io.emit("create post");
-    });
-});
-/* - - - End Twitter Clone - - - */
-
-/* - - - Start SES - - - */
-var sesRouter = require(__dirname + "/server-scripts/ses-webserver.js");
-app.use("/ses", sesRouter);
-/* - - - End SES - - - */
-
-// serve the css and js files
-app.get("/style/:file", (req, res) => {
-    res.sendFile(__dirname + "/style/" + req.params.file);
-});
-app.get("/scripts/:file", (req, res) => {
-    res.sendFile(__dirname + "/client-scripts/" + req.params.file);
-});
-
-// 404 error
-app.use(function (req, res, next) {
-    res.status(404).send("Sorry can't find that!")
-});
-
-http.listen(8080, () => {
-    console.log("Listening on port " + 8080);
+app.listen(process.env.port, () => {
+    console.log(`Listening on port ${process.env.port}.`);
 });
--- readme.md
+++
@@ -1,8 +0,0 @@
-This is the repository for my personal website.
-
-To run it:
-* Clone the repository.
-* Run `npm -i` to install the dependencies.
-* Run `node app.js` to run the server.
-
-The server runs on port 8080.

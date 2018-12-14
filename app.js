8c9ca34eb93eb521986f1dd1903704b557a976b1
ElementG9 <elementg9dev@gmail.com>
2018-12-13 18:21:16 -0700

    Added check that .env exists

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+if (!require("fs").existsSync(`${__dirname}/.env`)) {
+    console.error("Error: '.env' configuration file does not exist.");
+    process.exit(1);
+}
 require('dotenv').config();
+
 const app = require("express")();
+
 app.get("/", (req, res) => {
     res.send("<h1>Hello world!</h1>");
 });
+
 app.listen(process.env.port, () => {
     console.log(`Listening on port ${process.env.port}.`);
 });
